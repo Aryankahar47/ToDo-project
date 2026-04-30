0b98c4d137cf7446375c33097e7e2b00022b4594
Aryan Kahar <aryankahar463@gmail.com>
2026-04-30 19:24:59 +0530

    creating UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useEffect, useState } from "react";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function App() {
+//   const [todos, setTodos] = useState([]);
+
+//   // Fetch data from backend
+//   useEffect(() => {
+//     fetch("http://localhost:3001/todos")
+//       .then((res) => res.json())
+//       .then((data) => setTodos(data));
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Todo App</h1>
+
+//       {todos.length === 0 ? (
+//         <p>No Todos Found</p>
+//       ) : (
+//         todos.map((todo) => (
+//           <div key={todo.id}>
+//             <h3>{todo.title}</h3>
+//             <p>{todo.completed ? "✅ Completed" : "❌ Not Completed"}</p>
+//           </div>
+//         ))
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+function App(){
+  const [tasks, setTask] = useState(["Code", "Eat", "Sleep", "repeat"])
+  const [input, setInput] = useState("")
+  return(
+    <div className="list">
+      <h3 id="heading">Listings</h3>
+      <input type="text" placeholder="Enter Task" value={input} onChange={(e)=>setInput(e.target.value)}></input>
+      <button onClick={()=>{
+        if(input.trim()===""){
+          return
+        }
+        setTask([...tasks, input]);
+        setInput("");
+      }}>Add</button>
+      <ol>
+        {tasks.map((task, index)=>(
+          <li key={index} className="task-item">{task}</li>
+        ))}
+      </ol>
+
     </div>
-  );
+  )
+
 }
 
 export default App;
